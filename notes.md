8479a427255a70530ce12201af55bbe87fafff44
katredford <katharine.redford@gmail.com>
2021-07-13 13:39:25 -0500

    add auth

--- notes.md
+++ notes.md
@@ -3,3 +3,5 @@ npm i apollo-server-express graphql
 npm install bcrypt@5.0.1
 
 known issue with new version run : apollo-server-express@^2
+
+npm install jsonwebtoken
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const { ApolloServer } = require("apollo-server-express");
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
+const { authMiddleware } = require("./utils/auth");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
+	context: authMiddleware,
 });
 
 // integrate our Apollo server with the Express application as middleware
